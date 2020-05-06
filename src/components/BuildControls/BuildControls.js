86cda49f32c929cc825ba4bf53145e84f7200ce1
jkaddu <jkaddu@yahoo.com>
2020-05-05 21:29:10 -0700

    created buildControl/s folders/files

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const buildControls = (props) => {
+    <div className={classes.BuildControls}>
+        
+    </div>
+})
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,12 +6,13 @@ import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      bacon: 2,
-      cheese: 2,
-      meat: 1,
-      salad: 1,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+      salad: 0,
     },
   };
+
   render() {
     return (
       <Aux>
