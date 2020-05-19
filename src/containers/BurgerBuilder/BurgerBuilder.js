176e1e5ea333b5e5e42b3fedda01dd1b8b2fef01
jkaddu <jkaddu@yahoo.com>
2020-05-19 08:10:29 -0700

    added logo

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,10 +16,10 @@ const INGREDIENT_PRICES = {
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
+      salad: 0,
       bacon: 0,
       cheese: 0,
       meat: 0,
-      salad: 0,
     },
     totalPrice: 12,
     purchaseable: false,
