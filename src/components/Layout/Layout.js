b5e7bdcdc22c57b6dc8c992d3ee5ece5b7810732
jkaddu <jkaddu@yahoo.com>
2020-05-23 23:19:28 -0700

    added backdrop to sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.css";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <div>
-      <Toolbar />
-      <SideDrawer />
-    </div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-export default layout;
+  sideDrawerHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Toolbar drawer={this.sideDrawerHandler} />
+          <SideDrawer
+            open={this.state.showSideDrawer}
+            close={this.sideDrawerHandler}
+          />
+        </div>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 import classes from "./SideDrawer.css";
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="10%" />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show={props.open} click={props.close} />
+      <div className={attachedClasses.join(" ")}>
+        <Logo height="10%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
